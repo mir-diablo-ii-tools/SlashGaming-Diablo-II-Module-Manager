006236eb5daa8df946bf0acb41063f0fd49b83e9
Mir Drualga <IAmRealTrial+SGD2ModuleManager@gmail.com>
2022-03-06 15:17:02 -0800

    Add file count and file list operations

---
+++ SGD2ModuleManager/src/file/file_count.h
@@ -0,0 +1,38 @@
+/**
+ * SlashGaming Diablo II Module Manager
+ * Copyright (C) 2020-2022  Mir Drualga
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SGD2MODULEMANAGER_FILE_FILE_COUNT_H_
+#define SGD2MODULEMANAGER_FILE_FILE_COUNT_H_
+
+#include <stddef.h>
+#include <wchar.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+size_t File_CountFromDirectory(
+    const wchar_t* directory,
+    int (*validity_func)(const wchar_t* path));
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* SGD2MODULEMANAGER_FILE_FILE_COUNT_H_ */
