38123530075034b4cb371db31d36ab518a0a2853
Mir Drualga <IAmRealTrial+SGD2ModuleManager@gmail.com>
2022-03-13 03:48:45 -0700

    Add module manager public API

---
+++ SGD2ModuleManager/src/module_manager.c
@@ -0,0 +1,36 @@
+/**
+ * SlashGaming Diablo II Module Manager
+ * Copyright (C) 2020-2022  Mir Drualga
+ *
+ * This file is part of SlashGaming Diablo II Module Manager.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#include "../include/module_manager.h"
+
+#include "module_manager_internal.h"
+
+/**
+ * External
+ */
+
+void ModuleManager_GetFunction(
+    ModuleManager_FunctionType* functions,
+    const char* exported_name) {
+}
+
+void ModuleManager_GetFunctionsCount(const char* exported_name) {
+}
